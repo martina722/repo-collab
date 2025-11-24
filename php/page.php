e05d399add5a99c1e8d62883dac640466d13d117
lorel-halilaj <halilaj.lorel@gmail.com>
2025-11-24 16:33:05 +0100

    aggiunto php per la creazione di div

--- php/page.php
+++ php/page.php
@@ -10,6 +10,14 @@
 </head>
 <body>
     <h1>Pagina di prova</h1>
-
+    <div class="numerati">
+        <?php 
+        // dato un numero 15 crea 15 div numerati da 1 a 15
+        $numero_div = 15;
+        for ($i = 1; $i <= $numero_div; $i++) {
+            echo "<div>Questo è il div numero $i</div>";
+        }
+        ?>
+    </div>
     <div class="ultimo">Questa è la fine della pagina</div>
 </body>
