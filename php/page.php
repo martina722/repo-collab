b1a78a6cf9d1dbf1e62ea22565111a879cbc8a44
lorel-halilaj <halilaj.lorel@gmail.com>
2025-11-24 16:20:11 +0100

    initial php/html page

---
+++ php/page.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Page</title>
+    <link rel="stylesheet" href="page.css">
+</head>
+<body>
+    <h1>Pagina di prova</h1>
+
+    <div class="ultimo">Questa è la fine della pagina</div>
+</body>
