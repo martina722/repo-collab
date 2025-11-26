66525d3731930a8870d046563b92f5a38b219ea0
Lorel <halilaj.lorel@gmail.com>
2025-11-26 17:00:57 +0100

    fixato

--- colors/index.php
+++ colors/index.php
@@ -8,17 +8,12 @@
 </head>
 <body>
     <h1 class="rosso">colori</h1>
-    <div class="casuali">
         <?php
         $numero = rand(5, 30);
-        for ($i = 1; $i <= $numero; $i++) {
-            if ($i % 2 == 0) {
-                echo "<div> pari ($i) </div>";
-                echo "<div> dispari ($i)</div>"
-            }
+        for ($i = 1; $i < $numero; $i++) {
+                echo "<div class='casuali'></div>";
         }
         ?>
-    </div>
     <div class="end">fine pagina</div>
 </body>
 </html>
