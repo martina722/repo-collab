0a95097a81004e7b26ad20e453a8d1c540b540af
martina722 <marticm26@gmail.com>
2025-11-26 16:13:34 +0100

    modifiche

--- random2/index.php
+++ random2/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1 class="rosso">Contenuti a caso</h1>
@@ -20,6 +21,5 @@
     ?>
     </div>
     <div class="fine">fine pagina</div>
-
 </body>
 </html>
