ea29db7781ea102872d98792607cd3eca1c96869
martina722 <marticm26@gmail.com>
2025-11-26 16:09:36 +0100

    .

--- php/page.php
+++ php/page.php
@@ -25,7 +25,6 @@
 </html>
 
 
-
 <?php
 // Metodo con funzione
 
---
+++ random2/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="rosso">Contenuti a caso</h1>
+    <div class="casuali">
+    <?php
+    $numero = rand(10, 30);
+    for ($i = 1; $i <= $numero; $i++) {
+        if ($i % 2 == 0) {
+            echo "<div>io sono pari ($i)</div>";
+        } else {
+            echo "<div>io sono dispari ($i)</div>";
+        }
+    }
+    ?>
+    </div>
+    <div class="fine">fine pagina</div>
+
+</body>
+</html>
