47488c614c69b24420229f9802d8ca33e2ee3abd
martina722 <marticm26@gmail.com>
2025-12-03 16:13:05 +0100

    modifiche

---
+++ events/events.js
@@ -0,0 +1,5 @@
+var clickmeButton = document.getElementById("click-me-button")
+clickmeButton.addEventListener
+        box.style.backgroundColor = 
+        box.style.backgroundColor === "red" ? "lightgray" : "red";
+});
