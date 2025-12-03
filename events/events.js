353b146ebe3af0c15b7408188d3568c3ba151d56
Lorel <halilaj.lorel@gmail.com>
2025-12-03 16:35:16 +0100

    quasi fixato

--- events/events.js
+++ events/events.js
@@ -1,4 +1,5 @@
-var clickmeButton = document.getElementById("click-me-button")
+var clickmeButton = document.getElementById("click-me-button");
 clickmeButton.addEventListener ('click', function() {
-        clickmeButton.style.backgroundColor = "red"
+        var cambioColore = document.getElementById("giallo");
+        cambioColore.style.backgroundColor = "red";
 });
--- events/index.php
+++ events/index.php
@@ -1,6 +1,6 @@
 <?php
 function printDiv($text, $class) {
-    echo "<div class='" . $class . "'>" . $text . "</div>";
+    echo "<div id='giallo' class='" . $class . "'>" . $text . "</div>";
 };
 ?>
 
