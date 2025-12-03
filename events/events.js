b716ff784a48c6563e724c50ce53fd20f485e7eb
Lorel <halilaj.lorel@gmail.com>
2025-12-03 16:57:43 +0100

    formattatp per una miglior lettura

--- events/events.js
+++ events/events.js
@@ -2,6 +2,6 @@ var clickmeButton = document.getElementById("click-me-button");
 clickmeButton.addEventListener ('click', function() {
         var cambioColore = document.getElementsByClassName("giallo");
         for (i = 0; i < cambioColore.length; i++) {
-        cambioColore[i].style.backgroundColor = "red";
-}
+                cambioColore[i].style.backgroundColor = "red";
+        }
 });
