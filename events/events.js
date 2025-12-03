f039e04e12834a114287e959dad60dd8bb9cc4f1
Lorel <halilaj.lorel@gmail.com>
2025-12-03 17:20:18 +0100

    formattato file js

--- events/events.js
+++ events/events.js
@@ -1,7 +1,8 @@
 var clickmeButton = document.getElementById("click-me-button");
 clickmeButton.addEventListener ('click', function() {
-        var cambioColore = document.getElementsByClassName("giallo");
-        for (i = 0; i < cambioColore.length; i++) {
-                cambioColore[i].style.backgroundColor = "red";
+                var cambioColore = document.getElementsByClassName("giallo");
+                for (i = 0; i < cambioColore.length; i++) {
+                        cambioColore[i].style.backgroundColor = "red";
+                }
         }
-});
+);
