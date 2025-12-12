4f17760ff28193e3892d6ca0672a7b0819664bc5
Lorel <halilaj.lorel@gmail.com>
2025-12-12 12:50:56 +0100

    aggiunto commento per la funzione printDiv

--- lib/libreria.php
+++ lib/libreria.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Stampa un div con del testo e una classe specificata
+ * @param string $text Il testo da inserire nel div
+ * @param string $class La classe CSS da assegnare al div
+ * @return void
+ */
 function printDiv($text, $class) {
     echo "<div class='" . $class . "'>" . $text . "</div>";
 };
