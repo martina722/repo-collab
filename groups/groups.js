fd6608109d67edb73672648c7a7259bdb14f6a79
martina722 <marticm26@gmail.com>
2025-12-15 12:12:37 +0100

    modifiche

---
+++ groups/groups.js
@@ -0,0 +1,47 @@
+let clickmeButton = document.getElementById("primo-rosso");
+clickmeButton.addEventListener ('click', function() {
+                let div = document.getElementsByClassName('div')
+                for (i = 0; i < div.length; i++) {
+                        div[i].style.backgroundColor = "red";
+                }
+});
+
+clickmeButton = document.getElementById("secondo-rosso");
+clickmeButton.addEventListener ('click', function() {
+                let div1 = document.getElementsByClassName('div1')
+                for (i = 0; i < divs.length; i++) {
+                        div1[i].style.backgroundColor = "red";
+                }
+});
+
+clickmeButton = document.getElementById("rossi");
+clickmeButton.addEventListener ('click', function() {
+                let divs = document.getElementsByClassName('divs')
+                for (i = 0; i < div.length; i++) {
+                        divs[i].style.backgroundColor = "red";
+                }
+});
+
+clickmeButton = document.getElementById("primo-blu");
+clickmeButton.addEventListener ('click', function() {
+                let divs = document.getElementsByClassName('divs')
+                for (i = 0; i < div.length; i++) {
+                        divs[i].style.backgroundColor = "blue";
+                }
+});
+
+clickmeButton = document.getElementById("secondo-blu");
+clickmeButton.addEventListener ('click', function() {
+                let divs = document.getElementsByClassName('divs')
+                for (i = 0; i < div.length; i++) {
+                        divs[i].style.backgroundColor = "blue";
+                }
+});
+
+clickmeButton = document.getElementById("blu");
+clickmeButton.addEventListener ('click', function() {
+                let divs = document.getElementsByClassName('divs')
+                for (i = 0; i < div.length; i++) {
+                        divs[i].style.backgroundColor = "blue";
+                }
+});
