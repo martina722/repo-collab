d735fdd994cc27d520f5de6b12d569ce87a2021b
Lorel <halilaj.lorel@gmail.com>
2025-12-17 11:55:33 +0100

    implementati bottoni per terzo gruppo

--- groups/groups.js
+++ groups/groups.js
@@ -1,3 +1,30 @@
+function changeColor(buttonId, className, color) {
+    let clickmeButton = document.getElementById(buttonId);
+    clickmeButton.addEventListener('click', function() {
+        let divs = document.getElementsByClassName(className);
+        for (let i = 0; i < divs.length; i++) {
+            divs[i].style.backgroundColor = color;
+        }
+    });
+}
+
+changeColor("primo-rosso", "primo-gruppo", "red");
+changeColor("secondo-rosso", "secondo-gruppo", "red");
+changeColor("terzo-rosso", "terzo-gruppo", "red");
+changeColor("rossi", "tutti", "red");
+
+changeColor("primo-blu", "primo-gruppo", "blue");
+changeColor("secondo-blu", "secondo-gruppo", "blue");
+changeColor("terzo-blu", "terzo-gruppo", "blue");
+changeColor("blu", "tutti", "blue");
+
+changeColor("primo-giallo", "primo-gruppo", "yellow");
+changeColor("secondo-giallo", "secondo-gruppo", "yellow");
+changeColor("terzo-giallo", "terzo-gruppo", "yellow");
+changeColor("tutti-giallo", "tutti", "yellow");
+
+// Singoli bottoni
+ /*
 let clickmeButton = document.getElementById("primo-rosso");
 clickmeButton.addEventListener ('click', function() {
                 let div = document.getElementsByClassName('primo-gruppo')
@@ -45,3 +72,4 @@ clickmeButton.addEventListener ('click', function() {
                         div[i].style.backgroundColor = "blue";
                 }
 });
+*/
