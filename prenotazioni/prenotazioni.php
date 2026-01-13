fa99404311af167ced8ab727213bf7fc91f78287
Lorel <halilaj.lorel@gmail.com>
2026-01-13 12:26:44 +0100

    creati div per dettagli prenotazione

--- prenotazioni/prenotazioni.php
+++ prenotazioni/prenotazioni.php
@@ -11,19 +11,21 @@
         Prenotazioni
     </h1>
     <?php
-        require_once '../lib/libreria.php'
+        require_once '../lib/libreria.php';
 
-         //inizializza la connessione al database tramite funzione contenuta nella libreria libreria
-        $dbConnection = connectDatabase('cescot');
+        $dbConnection = connectDatabase('prenotazioni');
 
         //esegui la query che legge la tabella clienti
-        $query = 'SELECT * FROM clienti WHERE';
+        $query = 'SELECT clienti.nome, clienti.cognome, prenotazioni.arrivo, citta.citta, prenotazioni.importo, prenotazioni.caparra, prenotazioni.importo - prenotazioni.caparra AS saldo
+            FROM clienti
+            JOIN citta ON citta.id_citta = clienti.citta
+            JOIN prenotazioni  ON prenotazioni.cliente = clienti.id_cliente';
         $result = mysqli_query($dbConnection, $query);
 
         //ciclo sulle righe restituite e stampo il valore di ogni riga
         while ($row = mysqli_fetch_assoc($result)) {
-            $clientiDivContent = '<h2>' . $row['id'] . '</h2><p>' . $row['nome'] . ' ' . $row['cognome'] . '</p>';
-            printDiv($clientiDivContent, 'cliente');
+            $clientiDivContent = '<h2>' . $row['arrivo'] . '</h2><p>' . $row['nome'] . ' ' . $row['cognome'] . '<br>' . $row['citta'] . '<br> Importo prenotazione: ' . $row['importo'] . '<br> Caparra: ' . $row['caparra'] . '<br> Saldo: ' . $row['saldo'] . '</p>';
+            printDiv($clientiDivContent, 'prenotazione');
         }
     ?>
 </body>
