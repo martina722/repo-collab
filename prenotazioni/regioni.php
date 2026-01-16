753f26f71d1d0913655e386100ae2be964a64fc8
Lorel <halilaj.lorel@gmail.com>
2026-01-16 11:43:19 +0100

    modificato per capire meglio le query

--- prenotazioni/regioni.php
+++ prenotazioni/regioni.php
@@ -31,10 +31,8 @@
     JOIN prenotazioni ON prenotazioni.cliente = clienti.id_cliente
     GROUP BY regioni.regione';
     $result = mysqli_query($dbConnection, $query);
-
-    // se nome da cercare è stato inserito mostra i risultati 
-    if (!empty($regione_da_cercare)) {
-        $query1 = 'SELECT regioni.regione, COUNT(prenotazioni.id_prenotazione) AS totale_prenotazioni,
+    // query per ottenere le regioni filtrate in base al nome inserito
+    $query1 = 'SELECT regioni.regione, COUNT(prenotazioni.id_prenotazione) AS totale_prenotazioni,
         ROUND(SUM(prenotazioni.importo), 2) AS totale_importo,
         ROUND(SUM(prenotazioni.importo - prenotazioni.caparra), 2) AS totale_saldo
         FROM regioni
@@ -44,7 +42,10 @@
         WHERE regioni.regione = "' . $regione_da_cercare . '"
         GROUP BY regioni.regione';
 
-        $result1 = mysqli_query($dbConnection, $query1);
+    $result1 = mysqli_query($dbConnection, $query1);
+
+    // se nome da cercare è stato inserito mostra i risultati 
+    if (!empty($regione_da_cercare)) {
         while ($row = mysqli_fetch_assoc($result1)) {
             $regioniDivContent = '<h2>' . $row['regione'] . '</h2><p>Num. prenotazioni: ' . $row['totale_prenotazioni'] . '<br>importo totale: ' . $row['totale_importo'] . '<br>saldo totale: ' . $row['totale_saldo'] . '</p>';
             printDiv($regioniDivContent, 'cliente display-inline-block');
