5e5f6633d2eb8baff7e559d7eed4a9251df99777
Lorel <halilaj.lorel@gmail.com>
2026-03-18 12:38:34 +0100

    migliorato il layout della pagina di ricerca utenti per una migliore leggibilità

--- prenotazioni/ricerca-utenti.php
+++ prenotazioni/ricerca-utenti.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizza utenti</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1>Ricerca Utenti</h1>
     <form action="visualizza-utenti.php" method="get">
-        <label for="nome">cerca utente:</label>
+        <label for="nome">Cerca utente per nome:</label>
         <input type="text" name="nome" id="nome" required>
         <input type="submit" value="Cerca">
     </form>
